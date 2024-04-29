2d1674840fa84e90fc03dccc943a328c2476b9ff
hamedtarkashvand <hamed.tarkashvand@gmail.com>
2024-04-29 11:32:38 +0330

    qqqd

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return <h1 onClick={()=>setCount(count + 1)} className="text-red-500">counter : </h1>
+  return <h1 onClick={()=>setCount(1)} className="text-red-500">counter : </h1>
 }
-
-
-
-
-
 
 export default App
