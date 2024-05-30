e814daa4f552e3d86f8f1cbf1d3ec254eebcc854
Hamed <hamed@Hameds-MacBook-Air.local>
2024-05-30 17:23:48 +0330

    provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,6 @@ import {useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <h1 onClick={() => setCount(1)} className="text-red-500">
-      counter : {count}
-    </h1>
-  )
+  return <div></div>
 }
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {ThemeProvider} from '@mui/material/styles'
 import App from './App.tsx'
 import './index.css'
+import {theme} from './providers/thems'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <>
+      <ThemeProvider theme={theme} />
+      <App />
+    </>
   </React.StrictMode>,
 )
