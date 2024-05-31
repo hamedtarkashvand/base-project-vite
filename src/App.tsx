1fb02e07b570eb145892dda6697be3db696d7805
hamedtarkashvand <hamed.tarkashvand@gmail.com>
2024-06-01 02:08:13 +0330

    feat: final input

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,123 @@
+import {useForm} from 'react-hook-form'
+import TextField, {TextFieldProps} from '@mui/material/TextField'
+import {styled} from '@mui/system'
+
 import './App.css'
 
+import {Autocomplete, Button, Icon, IconButton, InputAdornment, PaletteOptions} from '@mui/material'
+import {AccountCircle, BorderColor} from '@mui/icons-material'
+import CheckCircleIcon from '@mui/icons-material/CheckCircle'
+import CancelIcon from '@mui/icons-material/Cancel';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import { useEffect } from 'react'
+import Input from './component/input'
+
+const Hamed = styled(TextField)<TextFieldProps>(({theme}) => {
+  const {primary , success , error } = theme.palette 
+  
+  return ({
+    // Root class for the input field
+
+    '& .MuiOutlinedInput-root': {
+      // Class for the border around the input field
+      '& .MuiOutlinedInput-notchedOutline': {
+         borderWidth: "1px",
+      },
+      "&.Mui-focused": {
+        "& .MuiOutlinedInput-notchedOutline": {
+          borderWidth: "2px",
+        },
+      },
+      '&:hover:not(.Mui-focused)': {
+        '& .MuiOutlinedInput-notchedOutline': {
+          borderColor: '#0000003b',
+        },
+      },
+    },
+    
+  })
+})
+
+type FormData = {
+  firstName: string;
+    name: string;
+};
+
 function App() {
-  return <div></div>
+  const {
+    register,
+    getValues,
+    trigger,
+    formState: {errors , dirtyFields },
+  } = useForm<FormData>({
+    defaultValues:{
+      firstName:'',
+      name:''
+    },
+    mode: 'all',
+  })
+
+  
+  useEffect(()=>{
+    console.log(dirtyFields);
+    
+  },[])
+
+
+  return (
+    <div
+      className="
+       max-w-[1920px] 
+    ">
+      <Hamed
+        className=""
+        fullWidth
+        variant="outlined"
+        color={dirtyFields['firstName'] ? 'success' : 'primary'}
+        label={'نام کاربری'}
+        {...register('firstName', {
+          required: {
+            value: true,
+            message: 'نام کاربری وارد کنید',
+          },
+        })}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start" disablePointerEvents={true}>
+              <AccountCircle />
+            </InputAdornment>
+          ),
+
+          endAdornment: (
+            !dirtyFields['firstName'] &&  !errors['firstName']  ? (<AccountCircle/>) :
+            (errors['firstName']? 
+            <ErrorOutlineIcon color="error" />:<CheckCircleIcon color='success' />  
+            ))
+          
+        }}
+        error={!!errors['firstName']}
+        placeholder="نام"
+        helperText={(errors['firstName']?.message as string) || 'نام کاربری'}
+      />
+
+      <Input id='name'
+      errors={errors} 
+      placeholder='محل وارد کردن نام'
+      register={register}
+      // registerOptions={{
+      //   required:{
+      //     value:true,
+      //     message:'نام کاربری اجباری است'
+      //   }
+      // }}
+      dirtyFields={Boolean(dirtyFields.name)}
+      helperText="این متن کمکی برای وارد کردن نام است"
+      startIcon={AccountCircle}
+      endIcon={AccountCircle}
+      required
+      />
+   
+    </div>
+  )
 }
 export default App
--- src/component/input.tsx
+++ src/component/input.tsx
@@ -1,8 +1,155 @@
-import type {FC} from 'react'
+import type {FC, HTMLInputTypeAttribute} from 'react'
+import {styled} from '@mui/system'
 
-interface inputProps {}
+import {
+  IconButton,
+  InputAdornment,
+  PaletteOptions,
+  SvgIconTypeMap,
+  TextFieldPropsColorOverrides,
+} from '@mui/material'
 
-const input: FC<inputProps> = ({}) => {
-  return <div>input</div>
+import TextField, {TextFieldProps} from '@mui/material/TextField'
+import {AccountCircle, BorderColor} from '@mui/icons-material'
+import CheckCircleIcon from '@mui/icons-material/CheckCircle'
+import CancelIcon from '@mui/icons-material/Cancel'
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline'
+import {
+  FieldErrors,
+  FieldValues,
+  RegisterOptions,
+  UseFormRegister,
+} from 'react-hook-form'
+import { OverridableComponent } from '@mui/material/OverridableComponent'
+
+interface inputProps {
+  id: string;
+  register:  UseFormRegister<FieldValues>;
+  registerOptions?: RegisterOptions ;
+  className?: string;
+  errors: FieldErrors;
+  dirtyFields: boolean;
+  required?: boolean;
+  helperText?: string;
+  placeholder?: string;
+  type?: HTMLInputTypeAttribute;
+  startIcon?: OverridableComponent<SvgIconTypeMap<{}, "svg">> 
+  endIcon?: OverridableComponent<SvgIconTypeMap<{}, "svg">> 
 }
-export default input
+type PropsHandelEndIcon = {
+  dirtyFields: boolean;
+  errors: boolean;
+  endIcon?: OverridableComponent<SvgIconTypeMap<{}, 'svg'>>;
+  registerOptions?: RegisterOptions;
+  required?: boolean;
+};
+
+const InputStyle = styled(TextField)<TextFieldProps>(({theme}) => {
+  const {primary, success, error} = theme.palette
+
+  return {
+    // Root class for the input field
+
+    '& .MuiOutlinedInput-root': {
+      // Class for the border around the input field
+      '& .MuiOutlinedInput-notchedOutline': {
+        borderWidth: '1px',
+      },
+      '&.Mui-focused': {
+        '& .MuiOutlinedInput-notchedOutline': {
+          borderWidth: '2px',
+        },
+      },
+      '&:hover:not(.Mui-focused)': {
+        '& .MuiOutlinedInput-notchedOutline': {
+          borderColor: '#0000003b',
+        },
+      },
+    },
+  }
+}) as typeof TextField
+
+const HandelEndIcon: React.FC<PropsHandelEndIcon> = ({
+  dirtyFields,
+  errors,
+  endIcon: EndIcon,
+  registerOptions,
+  required,
+}):JSX.Element | undefined | null => {
+
+
+  // if((!registerOptions || !required) && EndIcon) return ()
+  
+  if(registerOptions || required) {
+    if (!dirtyFields && !errors || EndIcon) {
+      return EndIcon ? <EndIcon  /> : null
+    } else if (errors) {
+      return <ErrorOutlineIcon color="error" />
+    } else {
+      return <CheckCircleIcon color="success" />
+    }
+  }
+
+console.log(registerOptions , required );
+
+  return EndIcon && <EndIcon  />
+}
+
+const Input: FC<inputProps> = ({
+  id,
+  register,
+  registerOptions,
+  className,
+  required,
+  errors ,
+  dirtyFields,
+  helperText,
+  type,
+  placeholder,
+  startIcon : StartIcon,
+  endIcon 
+}) => {
+  return (
+    <TextField
+      className={className}
+      required={required || !!registerOptions?.required}
+      fullWidth
+      type={type}
+      variant="outlined"
+      color={dirtyFields ? 'success' : 'primary'}
+      label={'نام'}
+      {...register(id, {
+        required,
+        ...registerOptions
+      })}
+
+      InputProps={{
+        startAdornment: StartIcon && (
+          <InputAdornment position="start" disablePointerEvents={true}>
+            <StartIcon />
+          </InputAdornment>
+        ),
+
+        endAdornment:(
+          <HandelEndIcon
+            dirtyFields={dirtyFields}
+            errors={!!errors[id]}
+            endIcon={endIcon}
+            registerOptions={registerOptions}
+            required={required}
+          />
+        ),
+
+        // endAdornment: (
+        //   (!dirtyFields && !errors['firstName']) ? (<AccountCircle/>) :
+        //   (errors['firstName']?
+        //   <ErrorOutlineIcon color="error" />:<CheckCircleIcon color='success' /> )
+        //   )
+      }}
+      error={Boolean(errors[id])}
+      placeholder={placeholder}
+      helperText={errors[id] ? errors[id].message as string : helperText}
+    />
+  )
+}
+export default Input
