67710a2d9c6ee5c21f4201f1378e206770088a0f
hamedtarkashvand <hamed.tarkashvand@gmail.com>
2024-06-03 15:52:31 +0330

    fix: back beranch main

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,6 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import styled from 'styled-components'
 import './App.css'
-import Books from './components/books/books'
-import Admin from './components/admin/admin'
-import Nav from './components/common/nav'
-import BooksList from './components/books/books-list'
-import Book from './components/books/book'
 
-const AppContainer = styled.div`
-  margin: 60px auto;
-  width: 400px;
-`
-const Content = styled.div`
-  background-color: white;
-  border-radius: 6px;
-  padding: 28px;
-  height: 100%;
-`
 function App() {
- 
-  return (
-    <>
-      <AppContainer>
-        <Router>
-          <Content>
-            <Nav />
-            <Routes>
-              <Route path="/" element={<Books />} >
-                <Route path='/' element={<BooksList/>} />
-                <Route path=':id' element={<Book/>} />
-
-              </Route>
-              <Route path="/admin" element={<Admin />} /> 
-              <Route path='*' element={<h2>oops! not found 404</h2>} />
-            </Routes>
-          </Content>
-        </Router>
-      </AppContainer>
-    </>
-  )
+  return <div></div>
 }
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {ThemeProvider} from '@mui/material/styles'
 import App from './App.tsx'
-import ThemeProvider from './providers/themProvider.tsx'
 import './index.css'
+import {theme} from './providers/thems'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <>
+      <ThemeProvider theme={theme} />
+      <App />
+    </>
+  </React.StrictMode>,
 )
